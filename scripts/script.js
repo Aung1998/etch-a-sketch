ebf466a9d78b43535f4e61c861640a9c6fed1688
Aung Myat Thu <aungmyatthu1998@gmail.com>
2021-06-20 13:28:18 +0630

    change to black background when howering in graph

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,7 @@
 const items = document.getElementsByClassName("item");
+
 for(let i = 0; i<=items.length; i++){
     items[i].addEventListener("mouseenter", function(e){
-        console.log(e);
+        items[i].classList.toggle("black");
     })
 }
