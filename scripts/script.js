2f122a5ebbea4d3ac78dd3bc7578eb1835880edb
Aung Myat Thu <aungmyatthu1998@gmail.com>
2021-06-20 15:19:31 +0630

    Modifiled js to remove unused css

--- scripts/script.js
+++ scripts/script.js
@@ -6,10 +6,13 @@ for(let i = 0; i<=items.length; i++){
     items[i].addEventListener("mouseenter", function(e){
         let name = getRandomString(5);
         var style = document.createElement('style');
+        style.id = name;
         style.innerHTML = `.${name} { traansform:translate(1); background-color:${randomHexColor()}; }`;
         document.getElementsByTagName('head')[0].appendChild(style);
         if((items[i].classList.length>1)){
             previous = items[i].classList[1];
+            document.getElementById(previous).remove();
+
         }
         items[i].classList.toggle(name);
         items[i].classList.remove(previous);
