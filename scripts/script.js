4cf9193fcd1d334e1b6095eebe8f7ba8fd1bef14
Aung Myat Thu <aungmyatthu1998@gmail.com>
2021-06-20 22:33:56 +0630

    added createGrid Function

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,12 @@
 const items = document.getElementsByClassName("item");
+const grid = document.getElementById("grid");
 
-for(let i = 0; i<items.length; i++){
-    items[i].addEventListener("mouseenter", function(e){
-       items[i].style.backgroundColor = `${randomHexColor()}`;
-    })
+function colorGrid(){
+    for(let i = 0; i<items.length; i++){
+        items[i].addEventListener("mouseenter", function(e){
+           items[i].style.backgroundColor = `${randomHexColor()}`;
+        })
+    }
 }
 
 function randomHexColor(){
@@ -12,3 +15,15 @@ function randomHexColor(){
     let randomHexColor = randomHex.padStart(6, 0);
     return '#'+randomHexColor;
 }
+
+function createGrid(squares) {
+    grid.style.setProperty('--rows', squares);
+    grid.style.setProperty('--cols', squares);
+    for (c = 0; c < (squares * squares); c++) {
+      let cell = document.createElement("div");
+      grid.appendChild(cell).className = "item";
+    }
+}
+
+createGrid(16);
+colorGrid();
